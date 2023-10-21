b78e342e62096c28f93b5a8625b4f39423157b4f
edinho <edmarsousamendes4@gmail.com>
2023-10-21 19:35:01 -0300

    docs: Add docs from Menu class

--- server/includes/Menu.hpp
+++ server/includes/Menu.hpp
@@ -5,7 +5,12 @@
 #include "Globals.hpp"
 #include <fstream>
 
-
+/**
+ * @class Server
+ * @brief Classe que representa e manipula o menu do servico.
+ *
+ * Esta classe ler o menu e implementa a logica para resuperar dados do sistema
+ */
 class Menu 
 {
     private:
@@ -15,8 +20,19 @@ class Menu
         void parse_file();
 
     public:
+        /**
+         * @brief Construtor padrão da classe Menu.
+         *
+         * Este construtor inicializa os membros de dados da classe e faz um parse do arquivo para json.
+         */
         Menu( std::string file_name );
 
+
+        /**
+         * @brief Este metodo retorna as messas registradas no sistema.
+         *
+         * @return Retorna um Json com os as messas registradas no sistema.
+         */
         json get_tables();
 };
 
